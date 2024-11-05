1cb962a38af939d8633ef83726ef5e943f010cf9
DESKTOP-2SI0R6A\Diana <dsarmientosf27@gmail.com>
2024-11-04 21:05:05 -0600

    Integracion de proyecto API a repositorio

---
+++ GestorEmpleados/Controllers/ProveedorController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MiWebAPI.Data;
+using MiWebAPI.Models;
+
+namespace MiWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProveedorController : ControllerBase
+    {
+        private readonly ProveedorData _proveedorData;
+        public ProveedorController(ProveedorData proveedorData)
+        {
+            _proveedorData = proveedorData;
+        }
+
+        [HttpPost]
+        [Route("GetProveedor")]
+        public async Task<IActionResult> Lista([FromBody] string filtro)
+        {
+            List<Proveedor> Lista = await _proveedorData.GetProveedor(filtro);
+            return StatusCode(StatusCodes.Status200OK, Lista);
+        }
+
+        [HttpPost]
+        [Route("AddProveedor")]
+        public async Task<IActionResult> AddProveedor([FromBody] Proveedor objeto)
+        {
+            var respuesta = await _proveedorData.AddProveedor(objeto);
+            return StatusCode(StatusCodes.Status200OK, new { isSuccess = respuesta });
+        }
+        [HttpPost]
+        [Route("DeleteProveedor")]
+        public async Task<IActionResult> DeleteProveedor([FromBody] int Id)
+        {
+
+            var respuesta = await _proveedorData.DeleteProveedor(Id);
+            return StatusCode(StatusCodes.Status200OK, new { isSuccess = respuesta });
+        }
+        [HttpPost]
+        [Route("UpdateProveedor")]
+        public async Task<IActionResult> UpdateProveedor([FromBody] Proveedor objeto)
+        {
+            var respuesta = await _proveedorData.UpdateProveedor(objeto);
+            return StatusCode(StatusCodes.Status200OK, new { isSuccess = respuesta });
+        }
+    }
+}
---
+++ GestorEmpleados/Models/Proveedor.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiWebAPI.Models
+{
+    public class Proveedor
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public int Edad { get; set; }
+        public string Empresa { get; set; }
+    }
+}
